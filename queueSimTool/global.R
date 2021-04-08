530575bad7795d228cf263be7f958b30e72f04f7
Tim Churches <tim.churches@gmail.com>
2021-04-08 11:13:46 +1000

    Tidied up prepDefaultData.R, made it idempotent, call it automatically if www dir is missing.

--- queueSimTool/global.R
+++ queueSimTool/global.R
@@ -7,21 +7,6 @@ library(purrr)
 library(shinyTime)
 library(lubridate)
 
-# Functions
-source_files <- c("helperFunctions.R", "arenaModel.R", "gpModel.R")
-for (source_file in source_files) {
-  source(paste("R/", source_file, sep = ""))
-}
-
-
-# Default data
-source_objects <- c("throughputP1", "utilisationP1", "processingP1", "throughputP2", "utilisationP2", "processingP2", 
-                    "infoThroughPut1", "infoThroughPut2", "infoProcessTime1", "infoProcessTime2")
-for (source_object in source_objects) {
-  load(paste("www/", source_object, sep = ""))
-}
-
-
 # Define theme colors
 color1 <- "aqua"
 colorHex1 <- '#00c0ef'
@@ -30,3 +15,23 @@ status1 <- 'info'
 color2 <- "green"
 colorHex2 <- '#00a65a'
 status2 <- 'success'
+
+# Functions
+source_files <- c("helperFunctions.R", "arenaModel.R", "gpModel.R")
+for (source_file in source_files) {
+  source(paste(here::here('queueSimTool/R/'), source_file, sep = ""))
+}
+
+
+# Default data
+source_objects <- c("throughputP1", "utilisationP1", "processingP1", "throughputP2", "utilisationP2", "processingP2", 
+                    "infoThroughPut1", "infoThroughPut2", "infoProcessTime1", "infoProcessTime2")
+if (file.exists(paste(here::here('queueSimTool/www/'), source_object[1], sep = ""))) {
+  
+  for (source_object in source_objects) {
+    load(paste(here::here('queueSimTool/www/'), source_object, sep = ""))
+  }
+} else {
+  source(paste(here::here('queueSimTool/R/'), 'prepDefaultData.R', sep = ""))
+}
+
